0095b719cb9f6b3bfaaeef2169369d140f9845f0
Tom Widdows <thomas.widdows10@gmail.com>
2024-03-25 15:27:54 +0000

    Add links to stylesheet and script

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,22 @@ function drawCircle() {
     ctx.arc(xCentre, yCentre, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "green";
     ctx.fill();
+    
+    c.addEventListener('click', (e) => {
+        var rect = e.target.getBoundingClientRect();
+        const clickPos = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+        };
+        clickCircle(clickPos.x, clickPos.y, xCentre, yCentre, radius);
+        if (clickCircle(clickPos.x, clickPos.y, xCentre, yCentre, radius)) {
+            console.log('circle clicked');
+        } else {
+            console.log('circle not clicked');
+        };
+    });
+}
+
+function clickCircle(clickX, clickY, xCircle, yCircle, radius) {
+    return Math.sqrt((clickX - xCircle) ** 2 + (clickY - yCircle) ** 2) < radius;
 }
