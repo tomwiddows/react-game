70e51ad333d5fae5b959a0dbe4a9782be92ea014
Tom Widdows <thomas.widdows10@gmail.com>
2024-04-05 02:51:33 +0000

    Add comments where necessary

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let score = 0;
 let displayScore = document.getElementById("score-id");
 let leaderboard = [];
 
+/** Determines the size of canvas depending on canvas_container width which is styled to 90vh. */
 function resizeCanvas() {
     const canvas = document.getElementById('canvas');
     const container = document.getElementById('canvas_container');
@@ -13,6 +14,7 @@ resizeCanvas();
 
 window.addEventListener('resize', resizeCanvas);
 
+/** Paints rules onto canvas.  */
 function howToPlay() {
     const c = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
@@ -20,17 +22,20 @@ function howToPlay() {
     ctx.font =  "18px Holtwood One SC";
     ctx.fillStyle = "green";
     ctx.fillText("The rules are simple:", 5, 23);
+    /** Wait 2.5 seconds before next line is shown */
     setTimeout(() => { ctx.fillText("click the green circle...", 5, 46); }, 2500);
     setTimeout(() => { ctx.fillText("as many times as you can...", 5, 69); }, 5000);
     setTimeout(() => { ctx.fillText("in 30 seconds!", 5, 92); }, 7500);
 }
 
+/** Calls nextTurn() and countdown(). Disables onClick events on all buttons. */
 function playGame() {
     let playButton = document.getElementById("play-button");
     let howToPlayButton = document.getElementById("how-to-play-button");
     let leaderboardButton = document.getElementById("leaderboard-button");
     console.log(displayScore);
     displayScore.innerHTML = score;
+    /** Scroll down to show full canvas */
     document.getElementById("score-id").scrollIntoView();
     nextTurn();
     countdown();
@@ -39,6 +44,7 @@ function playGame() {
     leaderboardButton.disabled = true;
 }
 
+/** Subtracts 1 from timer-id inner HTML every second. Calls endGame() if timer reaches 0. */
 function countdown() {
     let timer = document.getElementById("timer-id");
     let timeLeft = 30;
@@ -55,6 +61,7 @@ function countdown() {
 }, 1000);
 }
 
+/** Clears canvas and calls drawCircle(). */
 function nextTurn() {
     const c = document.getElementById("canvas");
     const ctx = c.getContext("2d");
@@ -62,6 +69,10 @@ function nextTurn() {
     drawCircle();
 }
 
+/** Draws a circle of radius 25px onto canvas centrered at a random (x,y) co-ordinate. 
+ * Listens for click event, checks if clickCircle() function is true.
+ * If click is within circle area, add 1 to score and call nextTurn().
+ * */
 function drawCircle() {
     radius=25;
     const c = document.getElementById("canvas");
@@ -86,22 +97,29 @@ function drawCircle() {
         clickCircle(clickPos.x, clickPos.y, xCentre, yCentre, radius);
         if (clickCircle(clickPos.x, clickPos.y, xCentre, yCentre, radius)) {
             console.log('circle clicked');
-            canvas.removeEventListener('click', handleClick); // Remove event listener to prevent multiple clicks
+            // Remove event listener to prevent multiple clicks
+            canvas.removeEventListener('click', handleClick); 
             score++;
             displayScore.innerHTML = score;
-            nextTurn(); // Start the next round if less than a minute has passed
+            nextTurn();
         } else {
             console.log('circle not clicked');
         };
     });
 }
 
+/** Calculates whether click was within area of circle */
 function clickCircle(xClick, yClick, xCircle, yCircle, radius) {
     distance = Math.sqrt((xClick - xCircle) ** 2 + (yClick - yCircle) ** 2);
     console.log(distance);
     return distance < radius;
 }
 
+/** Called when timer reaches 0.
+ * Paints GAME OVER on the canvas.
+ * Re-enables all buttons.
+ * Calls inputName() function.
+ */
 function endGame() {
     let playButton = document.getElementById("play-button");
     let howToPlayButton = document.getElementById("how-to-play-button");
@@ -119,6 +137,10 @@ function endGame() {
     inputName();
 }
 
+/** Prompts user to input their name to the leaderboard.
+ * Saves player name and score as an object in leaderboard array.
+ * Resets score to 0 for next game.
+ */
 function inputName() {
     let playerName = window.prompt("Enter your name: ");
     if (playerName !== null && playerName !== "") {
@@ -129,6 +151,9 @@ function inputName() {
     return;
 }
 
+/** Orders leaderboard array
+ * Draws leaderboard onto canvas.
+ */
 function displayLeaderboard() {
     const c = document.getElementById("canvas");
     const ctx = c.getContext("2d");
@@ -142,8 +167,8 @@ function displayLeaderboard() {
     for (let i = 0; i < leaderboard.length; i++) {
         const entry = leaderboard[i];
         const playerName = entry.Name; // Get the player's name
-        const score = entry.Score; // Get the player's score
-        const text = `${playerName}: ${score}`;
+        const lastScore = entry.Score; 
+        const text = `${playerName}: ${lastScore}`;
         ctx.fillText(text, 5, 23 * (i + 2));
     }
 }
